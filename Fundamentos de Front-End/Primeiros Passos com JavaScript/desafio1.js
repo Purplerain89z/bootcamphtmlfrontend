b8f869224b0f0847328865c4f31c2be18d5a7a66
Purplerain89z <almirlessa@gmail.com>
2023-12-17 20:17:07 -0300

    desafio basico

--- Fundamentos de Front-End/Primeiros Passos com JavaScript/desafio1.js
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/desafio1.js
@@ -1,4 +1,5 @@
-let km = 100, gastoCarro = 5, precoGas = 2.59 ;
-let total = (km *(gastoCarro*precoGas));
+let kmRodados = 100, gastoCarro = 2.5, precoGas = 2.59 ;
+let total = (kmRodados *(gastoCarro * precoGas));
+let litrosConsumidos = kmRodados / gastoCarro;
 
-console.log("Você vai viajar "+km+"km Seu carro gaasta "+ gastoCarro+" litros por KM, o total a pagar é "+total+".")
+console.log("Você viajou "+kmRodados+"km. Seu carro gaasta "+ gastoCarro+" litros por KM, o total consumido foi "+litrosConsumidos+", o preço da gasoline esta "+precoGas+" o total a pagar é "+total+".");
