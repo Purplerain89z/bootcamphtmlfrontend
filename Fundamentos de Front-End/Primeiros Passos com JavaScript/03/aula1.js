4066182eabbea207a4139fddf3124b7b735aea0e
Purplerain89z <almirlessa@gmail.com>
2023-12-18 11:25:43 -0300

    function exemplos

---
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/03/aula1.js
@@ -0,0 +1,20 @@
+// function teste(){
+// }
+// teste();
+// teste();
+// function sayMyName(name){
+//     console.log("Your Name is:" +name);
+// }
+// sayMyName("lala");
+// sayMyName("lulu");
+// function quadrado(valor){
+//     return valor * valor;
+// }
+// const quadradoDez = quadrado(10);
+// console.log(quadradoDez);
+function incrementarJuros(valor, percentual){
+    const valorDeAcressimo = (percentual / 100) * valor;
+    return valor + valorDeAcressimo;
+}
+
+console.log(incrementarJuros(100,10));
