fd036fe7fdb71638344505a2082b69ff7ceb8340
Purplerain89z <almirlessa@gmail.com>
2023-12-18 10:07:50 -0300

    imc

--- Fundamentos de Front-End/Primeiros Passos com JavaScript/02/exercicio3.js
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/02/exercicio3.js
@@ -1,21 +1,21 @@
-let peso = 85, altura= 185;
-const IMC = peso / (altura/altura);
+let peso = 85, altura= 1.85;
+const IMC = (peso / (altura * altura)).toFixed(2);
 
 console.log("Imc para Adultos:");
 
 if (IMC < 18.5){
-    console.log("Seu peso é "+IMC);
+    console.log("Seu IMC é "+IMC);
     console.log("Abaixo do peso.");
-}else if(IMC >= 18.5 || IMC <=25){
-    console.log("Seu peso é "+IMC);
+}else if(IMC >= 18.5 && IMC <=25){
+    console.log("Seu IMC é "+IMC);
     console.log("IMC sugere que você esta com Peso normal");
-}else if(IMC > 24 || IMC <31){
-    console.log("Seu peso é "+IMC);
+}else if(IMC > 24 && IMC <31){
+    console.log("Seu IMC é "+IMC);
     console.log("IMC sugere que você esta Acima do peso");
-}else if (IMC > 29 || IMC < 41){
-    console.log("Seu peso é "+IMC);
+}else if (IMC > 29 && IMC < 41){
+    console.log("Seu IMC é "+IMC);
     console.log("IMC sugere que você esta Obeso.")
 }else {
-    console.log("Seu peso é "+IMC);
+    console.log("Seu IMC é "+IMC);
     console.log("IMC sugere que você esta com Obesidade Grave.")
 }
