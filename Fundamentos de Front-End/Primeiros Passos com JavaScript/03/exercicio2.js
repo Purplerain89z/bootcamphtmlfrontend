1ab9ae73f8d23d028ef6e14c103b61b336452f8f
Purplerain89z <almirlessa@gmail.com>
2023-12-18 21:51:09 -0300

    funcoes

---
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/03/exercicio2.js
@@ -0,0 +1,15 @@
+function EscreverMeuNome(nome){
+    console.log("Meu nome é "+ nome);
+}
+
+EscreverMeuNome("Almir");
+
+function maiorIdade(idade){
+    if (idade > 17){
+        console.log("Maior de idade");
+    }else{
+        console.log("Menor de idade.");
+    }
+}
+
+maiorIdade(16);
