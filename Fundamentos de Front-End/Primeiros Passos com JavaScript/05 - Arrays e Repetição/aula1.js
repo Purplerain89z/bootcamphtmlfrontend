5acdc2dad1063a00744a36c300bd330747096026
Purplerain89z <almirlessa@gmail.com>
2023-12-21 17:07:50 -0300

    letra por letra

--- Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/aula1.js
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/aula1.js
@@ -1,17 +1,24 @@
-const notas = [];
-let i,soma = 0;
-notas.push(5);
-notas.push(7);
-notas.push(8);
-notas.push(2);
-notas.push(5);
+const nome = 'Vitor Jahansen Guerra';
 
 
-for (let i = 0;i < notas.length;i++){
-    soma = soma + notas[i]
+for (let i = 0;i < nome.length; i++){
+    console.log(nome[i]);
 }
 
-console.log(soma / notas.length);
+// const notas = [];
+// let i,soma = 0;
+// notas.push(5);
+// notas.push(7);
+// notas.push(8);
+// notas.push(2);
+// notas.push(5);
+
+
+// for (let i = 0;i < notas.length;i++){
+//     soma = soma + notas[i]
+// }
+
+// console.log(soma / notas.length);
 
 
 
