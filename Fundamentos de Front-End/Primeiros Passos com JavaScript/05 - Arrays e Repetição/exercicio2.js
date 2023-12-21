1c0696a59e415c0e22ab2c46f6e33cef2a931b64
Purplerain89z <almirlessa@gmail.com>
2023-12-21 18:29:33 -0300

    tabuada

--- Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/exercicio2.js
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/exercicio2.js
@@ -1,5 +1,8 @@
 let i,num = 8, resultado;
 
 for (let i=1; i <= 10;i++){
-        console.log(`${num} x ${i} = ${num*i}`);
+    for( let j=1; j<=10;j++ ){
+        console.log(`${j} x ${i} = ${j*i}`);
+    }
+    console.log('\n');
 }
