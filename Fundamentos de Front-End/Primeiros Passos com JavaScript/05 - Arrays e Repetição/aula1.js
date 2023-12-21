e7929b65a1df1f1951065060da8503b3e4652c9a
Purplerain89z <almirlessa@gmail.com>
2023-12-21 17:02:18 -0300

    Repetição media media

--- Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/aula1.js
+++ Fundamentos de Front-End/Primeiros Passos com JavaScript/05 - Arrays e Repetição/aula1.js
@@ -1,3 +1,21 @@
+const notas = [];
+let i,soma = 0;
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+
+for (let i = 0;i < notas.length;i++){
+    soma = soma + notas[i]
+}
+
+console.log(soma / notas.length);
+
+
+
+
 // const aluno =['João','Vitor','Marina'];
 
 // aluno.push('Renan');
