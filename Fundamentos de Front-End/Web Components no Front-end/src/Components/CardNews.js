b6b6019015453ee4c3da4b3154277aa2c821e497
Purplerain89z <almirlessa@gmail.com>
2024-01-08 22:46:06 -0300

    Desenvolvimento Frontend

--- Fundamentos de Front-End/Web Components no Front-end/src/Components/CardNews.js
+++ Fundamentos de Front-End/Web Components no Front-end/src/Components/CardNews.js
@@ -1,10 +1,25 @@
-class CardNews extends HTMLElement{
+class CardNews extends HTMLAnchorElement{
     constructor(){
         super();
-
-        const shadow = this.attachShadow({mode:"open"})
-        shadow.innerHTML = "<h1>Hello World</h1>"
+        const shadow = this.attachShadow({mode:"open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
     }
-}
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class","card");
 
-customElements.define('card-news',CardNews)
+        const cardLeft =  document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class","card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+        return componentRoot;
+    }
+
+    style(){}
+}
+customElements.define("card-news", CardNews);// colocar nome do elemento com traço
